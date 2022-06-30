bbb11cf5e9d89a24ca14d2c87dc6b894566af32a
TheLegendary1248 <1248alex@gmail.com>
2022-06-30 19:22:35 -0400

    Smoothed camera flip

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,21 +6,29 @@ public class CameraController : MonoBehaviour
 {
     public Camera cam;
     public float prevAspect;
+    public int framesSinceTurn;
+    public bool isSideways;
     private void FixedUpdate()
     {
-        if(cam.aspect != prevAspect)
+        bool aspectChange = cam.aspect != prevAspect;
+        if (aspectChange | framesSinceTurn < 60)
         {
+            if (aspectChange) framesSinceTurn = 0;
             if (cam.aspect < 1.22f)
             {
-                transform.eulerAngles = new Vector3(0, 0, 90f);
-                cam.orthographicSize = Mathf.Max(20f,16f / cam.aspect);
+                transform.eulerAngles = Vector3.Lerp(new Vector3(0, 0, 90f), transform.eulerAngles, 0.90f);
+                float size = Mathf.Max(20f, 16f / cam.aspect);
+                cam.orthographicSize = Mathf.Lerp(size, cam.orthographicSize, 0.90f);
+                isSideways = true;
             }
             else
             {
-                transform.eulerAngles = Vector2.zero;
-                cam.orthographicSize = 16f;
+                transform.eulerAngles = Vector3.Lerp(Vector3.zero, transform.eulerAngles, 0.90f);
+                cam.orthographicSize = Mathf.Lerp(16f, cam.orthographicSize, 0.90f);
+                isSideways = false;
             }
             prevAspect = cam.aspect;
+            framesSinceTurn++;
         }
         
     }
